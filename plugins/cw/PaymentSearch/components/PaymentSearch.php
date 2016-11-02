7b12bb18f90a172fafad380f412e1a3440f6ee6d
developer <developer@codeweavers.net>
2016-11-02 11:04:15 +0000

    Added basic request to payment search service.

--- plugins/cw/PaymentSearch/components/PaymentSearch.php
+++ plugins/cw/PaymentSearch/components/PaymentSearch.php
@@ -15,7 +15,56 @@ class PaymentSearch extends ComponentBase
 
     public function search()
     {
-        return 'Some Results';
+        $url = 'https://services.codeweavers.net/public/v2/JsonFinance/Calculate';
+
+        $request = [
+            'Credentials' => [
+                'ApiKey' =>  env('CW_API_KEY')
+            ],
+            'Customer' => [
+                'Reference' => "SESSIONID"
+            ],
+            'Parameters' => [
+                'Term' => "36",
+                'Deposit' => "10",
+                'DepositType' => "Percentage",
+                'AnnualMileage' => "10000"
+            ],
+            'VehicleRequests' => [
+                [
+                    'Id' => '1234',
+                    'Vehicle' => [
+                        'CashPrice' => "10000",
+                        'IsNew' => "true",
+                        'Identifier' => "CAP_ID",
+                        'IdentifierType' => "CapShortCode",
+                        'Type' => "Car",
+                        'StockId' => "12345",
+                        'ImageUrl' => "http://notworking.com/1234.jpg",
+                        'DealerVehicleUrl' => "http://notworking.com/1234/",
+                        'IsVatQualifying' => "true"
+                    ]
+                ]
+            ]
+        ];
+
+        $options = [
+            'http' => [
+                'header'  => [
+                    "Referer: http://localhost",
+                    "Content-type: application/json",
+                    "Accept: application/json",
+                    sprintf('Content-Length: %d', strlen(json_encode($request)))
+                ],
+                'method'  => 'POST',
+                'content' => json_encode($request),
+            ]
+        ];
+
+        $context  = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+
+        return $response;
     }
 
     public function onRun()
