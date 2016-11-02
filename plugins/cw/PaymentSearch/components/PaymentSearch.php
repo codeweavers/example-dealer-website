3d175a98f2fd8741fc2690169d033079ec0a705f
developer <developer@codeweavers.net>
2016-11-02 12:24:07 +0000

    Switched to payment search service.
    Outputted vehicle response on the listings page.

--- plugins/cw/PaymentSearch/components/PaymentSearch.php
+++ plugins/cw/PaymentSearch/components/PaymentSearch.php
@@ -1,10 +1,10 @@
 <?php namespace Cw\PaymentSearch\Components;
 
 use Cms\Classes\ComponentBase;
+use Illuminate\Support\Facades\URL;
 
 class PaymentSearch extends ComponentBase
 {
-
     public function componentDetails()
     {
         return [
@@ -15,11 +15,12 @@ class PaymentSearch extends ComponentBase
 
     public function search()
     {
-        $url = 'https://services.codeweavers.net/public/v2/JsonFinance/Calculate';
+        $url = 'https://services.codeweavers.net/public/v2/paymentsearch/search';
 
         $request = [
             'Credentials' => [
-                'ApiKey' =>  env('CW_API_KEY')
+                'ApiKey' => env('CW_API_KEY'),
+                "SystemKey" => env('CW_SYSTEM_KEY')
             ],
             'Customer' => [
                 'Reference' => "SESSIONID"
@@ -30,39 +31,36 @@ class PaymentSearch extends ComponentBase
                 'DepositType' => "Percentage",
                 'AnnualMileage' => "10000"
             ],
-            'VehicleRequests' => [
-                [
-                    'Id' => '1234',
-                    'Vehicle' => [
-                        'CashPrice' => "10000",
-                        'IsNew' => "true",
-                        'Identifier' => "CAP_ID",
-                        'IdentifierType' => "CapShortCode",
-                        'Type' => "Car",
-                        'StockId' => "12345",
-                        'ImageUrl' => "http://notworking.com/1234.jpg",
-                        'DealerVehicleUrl' => "http://notworking.com/1234/",
-                        'IsVatQualifying' => "true"
-                    ]
-                ]
+            'Search' => [
+                'MinimumPayment' => 100,
+                'MaximumPayment' => 1000,
+                'Query' => ""
+            ],
+            'Options' => [
+                'HasProductsGroupedByVehicle' => true,
+                'ReturnErrors' => false,
+                'SortBy' => "RegularPaymentDescending",
+                'PageNumber' => 1,
+                'ResultsPerPage' => 50,
+                'IncludeVehiclesWithNoDescription' => "true"
             ]
         ];
 
         $options = [
             'http' => [
-                'header'  => [
-                    "Referer: http://localhost",
+                'header' => [
+                    "Referer: " . URL::to('/'),
                     "Content-type: application/json",
                     "Accept: application/json",
                     sprintf('Content-Length: %d', strlen(json_encode($request)))
                 ],
-                'method'  => 'POST',
+                'method' => 'POST',
                 'content' => json_encode($request),
             ]
         ];
 
-        $context  = stream_context_create($options);
-        $response = file_get_contents($url, false, $context);
+        $context = stream_context_create($options);
+        $response = json_decode(file_get_contents($url, false, $context));
 
         return $response;
     }
